cf035e0f86dbfb0d3335f2264d91539ea84bc2a1
Thomas1379 <thomasdraghi@gmail.com>
2024-02-05 19:09:55 +0100

    login test2

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Participant;
+use App\Repository\CampusRepository;
+use PhpParser\Node\Expr\New_;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('plainPassword', PasswordType::class, [
+                                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('campus',ChoiceType::class, [
+                'choices' => [
+                    'Maybe' => null,
+                    'Yes' => true,
+                    'No' => false,
+                ],
+                // "name" is a property path, meaning Symfony will look for a public
+                // property or a public method like "getName()" to define the input
+                // string value that will be submitted by the form
+                 'choice_value' => 'nom',
+                // a callback to return the label for a given choice
+                // if a placeholder is used, its empty value (null) may be passed but
+                // its label is defined by its own "placeholder" option
+                'choice_label' => function (Participant $campus): Participant {
+                    return $campus;
+                },
+                // returns the html attributes for each option input (may be radio/checkbox)
+                'choice_attr' => function ($campus): array {
+                    return ['salut'];
+
+                },
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
