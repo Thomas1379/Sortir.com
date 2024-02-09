2a2c2196511abd83a36d16843e5ac0f5c0ce52db
Thomas1379 <thomasdraghi@gmail.com>
2024-02-09 17:55:51 +0100

    modif tableau ville

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -21,45 +21,24 @@ class VilleController extends AbstractController
         VilleRepository $villeRepository
     ): Response
     {
+        // Création d'une nouvelle ville
         $ville = new Ville();
         $villeForm = $this->createForm(VilleType::class, $ville);
-
         $villeForm->handleRequest($request);
 
+        // Enregistrement dans la bdd si rempli et validé
         if ($villeForm->isSubmitted() && $villeForm->isValid()) {
             $entityManager->persist($ville);
             $entityManager->flush();
 
+            // Affichage du message "succes" en cas de validation
             $this->addFlash('success', $ville->getNom() . " a bien été créée !");
             return $this->redirectToRoute('app_ville_index');
         }
 
+        // Affichage de la liste des villes créées et du formulaire de création
         return $this->render('ville/index.html.twig', [
             'villes' => $villeRepository->findAll(),
-            'villeForm' => $villeForm
-        ]);
-    }
-
-    #[Route('/new', name: 'app_ville_new')]
-    public function new(
-        Request $request,
-        EntityManagerInterface $entityManager
-    ): Response
-    {
-        $ville = new Ville();
-        $villeForm = $this->createForm(VilleType::class, $ville);
-
-        $villeForm->handleRequest($request);
-
-        if ($villeForm->isSubmitted() && $villeForm->isValid()) {
-            $entityManager->persist($ville);
-            $entityManager->flush();
-
-            $this->addFlash('success', $ville->getNom() . " a bien été créée !");
-            return $this->redirectToRoute('app_ville_index');
-        }
-
-        return $this->render('ville/new.html.twig', [
             'villeForm' => $villeForm
         ]);
     }
@@ -67,23 +46,31 @@ class VilleController extends AbstractController
     #[Route('/{id}', name: 'app_ville_show', methods: ['GET'])]
     public function show(Ville $ville): Response
     {
+        // Affichage le détails sur la ville
         return $this->render('ville/show.html.twig', [
             'ville' => $ville,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_ville_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Ville $ville, EntityManagerInterface $entityManager): Response
+    public function edit(
+        Request $request,
+        Ville $ville,
+        EntityManagerInterface $entityManager
+    ): Response
     {
+        // Modification d'une ville (nom ou code postal)
         $form = $this->createForm(VilleType::class, $ville);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
+            // Retour à la liste des villes après modification
             return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
         }
 
+        // Affichage de la liste des villes, avec la ville modifié (et toujours du formulaire de création)
         return $this->render('ville/edit.html.twig', [
             'ville' => $ville,
             'villeForm' => $form,
@@ -91,13 +78,18 @@ class VilleController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_ville_delete', methods: ['POST'])]
-    public function delete(Request $request, Ville $ville, EntityManagerInterface $entityManager): Response
+    public function delete(
+        Request $request,
+        Ville $ville,
+        EntityManagerInterface $entityManager
+    ): Response
     {
+        // Suppression d'une ville
         if ($this->isCsrfTokenValid('delete'.$ville->getId(), $request->request->get('_token'))) {
             $entityManager->remove($ville);
             $entityManager->flush();
         }
-
+        // Retour à la liste des villes après suppression
         return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
     }
 
