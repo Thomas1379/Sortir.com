ea77c4a68f5867c0da1aa29620c9107def3daa46
Fugann <95033152+Fugann@users.noreply.github.com>
2024-02-13 23:04:22 +0100

    add css + changements de quelques lignes un peu partout

--- src/Repository/VilleRepository.php
+++ src/Repository/VilleRepository.php
@@ -21,21 +21,27 @@ class VilleRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Ville::class);
     }
-  
+
+    public function getAllVille()
+    {
+        $queryBuilder = $this->createQueryBuilder('v');
+        $queryBuilder->addOrderBy('v.nom', 'DESC');
+        $query = $queryBuilder->getQuery();
+        $query->setMaxResults(500);
+        $results = $query->getResult();
+        return $results;
+    }
+
     //Recherche par nom ou par code Postal
     public function searchByNomOrCodePostal($searchTerm)
     {
         $queryBuilder = $this->createQueryBuilder('v');
-
-        if ($searchTerm) {
-            $queryBuilder
-                ->andWhere('v.nom LIKE :searchTerm OR v.codePostal LIKE :searchTerm')
-                ->setParameter('searchTerm', '%' . $searchTerm . '%');
-        }
+        $queryBuilder
+            ->where('v.nom LIKE :searchTerm OR v.codePostal LIKE :searchTerm')
+            ->setParameter('searchTerm', '%' . $searchTerm . '%');
 
         return $queryBuilder->getQuery()->getResult();
     }
-
 
     // Recherche si une ville (avec le même nom et le même code postal) existe déjà
    /* public function findByNomAndCodePostal(string $nom, string $codePostal): ?Ville
